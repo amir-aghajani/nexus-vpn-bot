8ea2eee21dd3e57ea748f6955306bf47275e5470
Amir Aghajani <amir20001382115@icloud.com>
2025-08-05 21:24:23 +0200

    Made some updates

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from config import settings
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+app = ApplicationBuilder().token(settings.telegram_bot_token).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
